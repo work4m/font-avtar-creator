4e8a2a1e12a1c3a9d2523a203f1e1530be544577
work4m <workingformix@gmail.com>
2021-11-19 20:32:28 +0530

    - get first image from server

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,26 @@
-import { Request, Response, Router } from "express";
+import { Request, Response, Router, NextFunction } from "express";
+import { generate } from "text-to-image";
 
 const router = Router();
 
-router.get('/:id', (req: Request, res: Response, next): Response => {
-    return res.send(req.params);
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    const base64Image = await generate('Lo', {
+        debug: false,
+        maxWidth: 64,
+        fontSize: 18,
+        customHeight: 64,
+        textAlign: "center",
+        verticalAlign: "center",
+        margin: 5,
+        bgColor: 'blue',
+        textColor: 'red',
+    });
+
+    const img = Buffer.from(base64Image.split(",")[1], 'base64');
+
+    res.set("Content-Type", "image/png");
+    res.set("Content-Length", img.length.toString());
+    res.send(img);
 });
 
 router.get('/:id/:name', (req: Request, res: Response, next): void => {
